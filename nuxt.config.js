8390fd796c0a74623b6918b077439e007a9a435d
Theo Ojochegbe <symplytheo@gmail.com>
2022-02-06 11:37:41 +0100

    refactor: setup theme icons & colors using vuetify

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,9 @@
-import colors from 'vuetify/es5/util/colors'
-
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+
+  // Disable Server Side rendering
+  ssr: false,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
@@ -17,7 +18,12 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+      { rel: 'preconnect',  href: 'https://fonts.gstatic.com', crossorigin: true },
+      { rel: 'stylesheet',  href: 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700&display=swap' },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
@@ -43,20 +49,9 @@ export default {
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
-    theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3,
-        },
-      },
-    },
+    optionsPath: '~/plugins/vuetify.config.js',
+    defaultAssets: false,
+    treeShake: true,
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
---
+++ plugins/vuetify.config.js
@@ -0,0 +1,28 @@
+const theme = {
+  dark: false,
+  themes: {
+    light: {
+      primary: '#7F56D9',
+      success: '#027A48'
+    },
+  },
+}
+
+const breakpoint = {
+  mobileBreakpoint: 'sm',
+  thresholds: {
+    xs: 576,
+    sm: 768,
+    md: 1024,
+    lg: 1200,
+  },
+  scrollBarWidth: 16,
+}
+
+export default {
+  theme,
+  breakpoint,
+  icons: {
+    iconfont: 'mdiSvg',
+  },
+}
